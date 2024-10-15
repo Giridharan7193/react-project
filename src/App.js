e94f01293b48667eca22ac68f9f52b1750557fd0
Giridharan7193 <giridharan7193gs@gmail.com>
2024-10-15 15:38:10 +0530

    second-commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          React
+          Documentry of Kamal
         </a>
       </header>
     </div>
